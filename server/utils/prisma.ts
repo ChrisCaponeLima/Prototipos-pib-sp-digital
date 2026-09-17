eab4d269622da8fb247f6fa951b87fd4b199763a
Chris Lima <127784737+ChrisCaponeLima@users.noreply.github.com>
2026-09-17 20:06:04 -0300

    pmo area

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,8 +1,9 @@
-import { PrismaClient } from '@prisma/client' 
-    const globalForPrisma = globalThis as unknown as { 
-        prisma: PrismaClient | undefined 
-    } 
-    
-    export const prisma = globalForPrisma.prisma ?? new PrismaClient() 
-    
-    if (process.env.NODEENV !== 'production') globalForPrisma.prisma = prisma
+import pkg from '@prisma/client'
+const { PrismaClient } = pkg
+
+const globalForPrisma = globalThis as unknown as {
+prisma: InstanceType<typeof PrismaClient> | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient() 
+if (process.env.NODE\_ENV !== 'production') globalForPrisma.prisma = prisma
